e4130477f9df9844496e875a5750141dc734becb
Jonathan Williamson <jonnywilliamson@gmail.com>
2024-02-19 11:02:56 +0000

    Update the prompt

--- app/OpenAI/Prompt.php
+++ app/OpenAI/Prompt.php
@@ -8,18 +8,24 @@ class Prompt
     {
         return [
             [
-                'role' => 'user',
-                'content' => "An array of NOTAM Tags, each tag has three columns: 'Tag Code', 'Tag Name', 'Tag Description': \n\n".Tags::all()->__toString(),
+                'role'    => 'system',
+                'content' => 'You are a NOTAM Librarian and categoriser who can decode and organise NOTAMs. Your replies are always json objects with no formatting.',
             ],
             [
-                'role' => 'user',
+                'role'    => 'assistant',
+                'content' => "An array of NOTAM Tags, each tag has three columns: 'Code', 'Tag Name', 'Tag Description': \n\n".Tags::all()->__toString(),
+            ],
+            [
+                'role'    => 'user',
                 'content' => <<<'EOL'
-You are a NOTAM Librarian. I will give you a number of NOTAM messages. Each start with an identity key then a colon.
-Always create an array of JSON objects with the following 4 fields per notam:
-"key": The notam identity key.
-"TagName": Choose the most logical Tag for this NOTAM from the list of Tags.
-"TagCode": The code for the selected Tag Name.
-"Explanation": In very simple English only, explain the NOTAM in a maximum of seven words, use sentence case but do not use abbreviations.
+I will give you a json_encoded NOTAM message. Each notam should be identified using the `key` field. The content uses the `all` field.
+Create a json object with exactly these 4 properties:
+"key": The notam `key` field.
+"type": Choose the most logical Tag for this NOTAM from the list of previous defined tags.
+"code": The code for the selected Tag Name.
+"summary": In very simple English only, explain the NOTAM in a maximum of seven words, use sentence case but do not use abbreviations.
+
+Do not use any formatting and ensure a valid json object is returned.
 EOL,
             ],
         ];
