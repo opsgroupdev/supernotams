2d031e56f831146006740d807306acd912cad068
Jonathan Williamson <jonnywilliamson@gmail.com>
2024-02-20 23:03:43 +0000

    tweak layout

--- resources/js/Pages/Notams/NotamsIndex.vue
+++ resources/js/Pages/Notams/NotamsIndex.vue
@@ -135,7 +135,7 @@ const toggleDetails = (notamId) => {
             </div>
         </form>
 
-        <Modal :show="result !== '' || progressMessage.length > 0" @close="closeModal" max-width="3xl">
+        <Modal :show="result !== '' || progressMessage.length > 0" @close="closeModal" max-width="6xl">
             <div v-if="fileKey !== ''" class="p-4 w-1/2 mx-auto bg-yellow-300 flex justify-center items-center m-3 rounded underline text-blue-800">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 mr-3">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15M9 12l3 3m0 0l3-3m-3 3V2.25" />
@@ -169,7 +169,7 @@ const toggleDetails = (notamId) => {
                                 <tbody>
                                 <template v-for="notam in notams">
                                     <tr :class="tableColour(notamSection, category)">
-                                        <td class="px-6 py-3 cursor-pointer text-blue-600 underline"
+                                        <td class="px-6 py-3 cursor-pointer whitespace-nowrap text-blue-600 underline"
                                             @click="toggleDetails(notam.id)">
                                             {{ notam.id }}
                                         </td>
