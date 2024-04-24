3fdde9a5692af073637d5a0097ec0977cc0d53b2
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2024-04-24 22:59:40 +0200

    Setup app layout and Livewire

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>
+        @if(isset($title))
+            {{ $title }} –
+        @endif
+        {{ config('app.name', 'Laravel') }}
+    </title>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css'])
+</head>
+<body class="font-sans antialiased">
+<div class="h-screen flex flex-col bg-gray-100 dark:bg-gray-900">
+    <x-navigation />
+
+    <!-- Page Heading -->
+    @if (isset($header))
+        <header class="bg-white dark:bg-gray-800 shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+    @endif
+
+    <!-- Page Content -->
+    <main {{ $attributes->class('flex-auto flex flex-col') }}>
+        {{ $slot }}
+    </main>
+</div>
+
+@stack('scripts')
+</body>
+</html>
